3fc64ddff0614e1dddd637a9d412e41e377e55c9
unknown <sinanazari110@gmail.com>
2022-11-06 20:44:25 +0430

    Assignment BMI

---
+++ lib/utils/calculate_bmi.dart
@@ -0,0 +1,36 @@
+import 'dart:math';
+
+class BmiLogic {
+  BmiLogic({this.height, this.weight});
+
+  final int height;
+  final int weight;
+
+  double _bmi;
+
+  String calculateBMI() {
+    _bmi = weight / pow(height / 100, 2);
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String getResult() {
+    if (_bmi >= 25) {
+      return 'Overweight';
+    } else if (_bmi > 18.5) {
+      return 'Normal';
+    } else {
+      return 'Underweight';
+    }
+  }
+
+  String getInterpretation() {
+    if (_bmi >= 25) {
+      return 'Damn son';
+    } else if (_bmi >= 18.5) {
+      return 'Not bad kid';
+    } else {
+      return 'African boy wassup ?'
+          '. ';
+    }
+  }
+}
